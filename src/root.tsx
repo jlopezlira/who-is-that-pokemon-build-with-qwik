9b7be07f618b568459aa56cc9add1201d1ce217f
Juan José López Lira <jlopezlira@gmail.com>
2023-07-17 21:46:49 -0500

    chore(package.json): add autoprefixer, postcss, and tailwindcss dependencies to enable Tailwind CSS support feat(postcss.config.js): add postcss configuration file to enable Tailwind CSS and autoprefixer plugins chore(global.css): import Tailwind CSS and apply base, components, and utilities styles fix(root.tsx): fix import order to import global.css before other components to ensure styles are applied correctly refactor(index.tsx): remove unused imports and update component structure fix(layout.tsx): fix import order to import Header component before other components to ensure correct rendering feat(tailwind.config.js): add tailwind.config.js file to customize Tailwind CSS configuration

--- src/root.tsx
+++ src/root.tsx
@@ -1,12 +1,13 @@
-import { component$ } from "@builder.io/qwik";
+import "./global.css";
+
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
-import { RouterHead } from "./components/router-head/router-head";
 
-import "./global.css";
+import { RouterHead } from "./components/router-head/router-head";
+import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   /**
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,10 +1,9 @@
-import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { routeLoader$ } from "@builder.io/qwik-city";
-import type { RequestHandler } from "@builder.io/qwik-city";
+import { Slot, component$, useStyles$ } from "@builder.io/qwik";
 
-import Header from "~/components/starter/header/header";
 import Footer from "~/components/starter/footer/footer";
-
+import Header from "~/components/starter/header/header";
+import type { RequestHandler } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import styles from "./styles.css?inline";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
